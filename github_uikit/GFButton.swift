af8678b642ceb45c974a39431bc1ea3678c28615
venn <vennboddula@gmail.com>
2024-04-10 22:19:24 +0530

    Added Generic Button and Textfield, Configured SearchVC

---
+++ github_uikit/GFButton.swift
@@ -0,0 +1,40 @@
+//
+//  GFButton.swift
+//  github_uikit
+//
+//  Created by Venkatesham Boddula on 06/04/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    /// This init is for StoryBoard.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    
+    private func configure() {
+        
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
--- github_uikit/SeachVC.swift
+++ github_uikit/SeachVC.swift
@@ -8,10 +8,86 @@
 import UIKit
 
 class SeachVC: UIViewController {
-
+    
+    let imageView = UIImageView()
+    let userNameTextField = GFTextField()
+    let gfButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBlue
+        
+        view.backgroundColor = .systemBackground
+        
+        configureImageView()
+        configureTextFieldView()
+        configureButtonView()
+        configureOnTapGesture()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+        
+    }
+    
+    func configureImageView() {
+        
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "gh-logo")
+        
+        NSLayoutConstraint.activate(
+            [
+                imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
+                imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                imageView.heightAnchor.constraint(equalToConstant: 100),
+                imageView.widthAnchor.constraint(equalToConstant: 100)
+            ])
+    }
+    
+    func configureTextFieldView() {
+        
+        view.addSubview(userNameTextField)
+        userNameTextField.delegate = self
+        NSLayoutConstraint
+            .activate(
+                [
+                    userNameTextField.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 50),
+                    userNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+                    userNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+                    userNameTextField.heightAnchor.constraint(equalToConstant: 50)
+                ])
+    }
+    
+    func configureButtonView() {
+        
+        view.addSubview(gfButton)
+        NSLayoutConstraint
+            .activate(
+                [
+                    gfButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+                    gfButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+                    gfButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+                    gfButton.heightAnchor.constraint(equalToConstant: 40)
+                ])
+    } 
+    
+    func configureOnTapGesture() {
+        
+        let tapGesture = UITapGestureRecognizer(
+            target: self.view, action: #selector(UIView.endEditing(_:)))
+        view.addGestureRecognizer(tapGesture)
+        
+    }
+}
+
+
+extension SeachVC: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        return true
+    }
+    
 }
